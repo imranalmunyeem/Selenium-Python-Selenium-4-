bfd9ea6a43e9b67c23cf556e307f2f70a59ca01c
IMRAN AL MUNYEEM <ialmunyeem@gmail.com>
2022-09-11 13:49:09 +0600

    Headless

--- Web Automation/Headless Testing.py
+++ Web Automation/Headless Testing.py
@@ -7,6 +7,7 @@
 #Selenium 4
 from selenium import webdriver
 
+#--------------------------------------Chrome Browser----------------------------------------#
 def headless_chrome():
     from selenium.webdriver.chrome.service import Service
     service_Object = Service("C:\\Users\\ialmu\\PycharmProjects\\Drivers\\chromedriver.exe")
@@ -16,6 +17,38 @@ def headless_chrome():
     return driver
 
 driver=headless_chrome()
+driver.get("https://demo.nopcommerce.com/")
+print(driver.title)
+print(driver.current_url)
+driver.close()
+
+
+#--------------------------------------Mozila Firefox Browser----------------------------------------#
+def headless_edge():
+    from selenium.webdriver.edge.service import Service
+    service_Object = Service("C:\\Users\\ialmu\\PycharmProjects\\Drivers\\IEDriverServer.exe")
+    option=webdriver.EdgeOptions()
+    option.headless=True
+    driver =webdriver.Edge(service = service_Object,options=option)
+    return driver
+
+driver=headless_edge()
+driver.get("https://demo.nopcommerce.com/")
+print(driver.title)
+print(driver.current_url)
+driver.close()
+
+
+#--------------------------------------IE Browser----------------------------------------#
+def headless_firefox():
+    from selenium.webdriver.firefox.service import Service
+    service_Object = Service("C:\\Users\\ialmu\\PycharmProjects\\Drivers\\geckodriver.exe")
+    option=webdriver.FirefoxOptions()
+    option.headless=True
+    driver =webdriver.Firefox (service = service_Object,options=option)
+    return driver
+
+driver=headless_firefox()
 driver.get("https://demo.nopcommerce.com/")
 print(driver.title)
 print(driver.current_url)
